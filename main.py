d5a2cb367c2e842ec6fe613c984f507c572afd4c
xiweiwong <133933950+xiweiwong@users.noreply.github.com>
2026-03-21 18:07:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ cc = OpenCC('s2t')
 # 💡 新增：偵測 GitHub Actions 觸發事件 MANUAL_ONLY
 GITHUB_EVENT = os.getenv('GITHUB_EVENT_NAME', 'local')
 if GITHUB_EVENT == 'workflow_dispatch':
-    RUN_MODE = "MANUAL_ONLY"
+    RUN_MODE = "FULL_SCAN"
 else:
     RUN_MODE = "FULL_SCAN"
 
